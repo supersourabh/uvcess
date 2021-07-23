733b1e9391501e161f34cfb6b1d96dcae5289717
supersourabh <kotagisourabh81@gmail.com>
2021-07-23 19:26:52 +0530

    uvcess

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import Student from "../models/studentModel";
 import { getToken } from "../utils";
 
@@ -6,25 +6,23 @@ const userRouter = express.Router()
 
 
 
-userRouter.post("/signup" , async (req ,res)=>{
+userRouter.post("/signup", async (req, res) => {
     const name = req.body.name
     const rollNo = req.body.rollNo
     const cr = req.body.cr
     const branch = req.body.branch
     const contact = req.body.contact
     const sem = req.body.sem
-    const admin =true
 
 
     try {
         const student = new Student({
-            name : name,
-            rollNo : rollNo,
-            branch : branch,
-            cr : cr,
-            sem:sem,
-            contact:contact&&contact,
-            isAdmin : admin
+            name: name,
+            rollNo: rollNo,
+            branch: branch,
+            cr: cr,
+            sem: sem,
+            contact: contact && contact,
         })
 
         const newUser = await student.save()
@@ -32,52 +30,52 @@ userRouter.post("/signup" , async (req ,res)=>{
 
 
         res.send({
-            _id:newUser._id,
-            name : newUser.name,
-            rollNo : newUser.rollNo,
-            branch : newUser.branch,
-            cr : newUser.cr,
-            sem :newUser.sem,
-            isAdmin : newUser.isAdmin,
-            token :getToken(newUser)
+            _id: newUser._id,
+            name: newUser.name,
+            rollNo: newUser.rollNo,
+            branch: newUser.branch,
+            cr: newUser.cr,
+            sem: newUser.sem,
+            isAdmin: newUser.isAdmin,
+            token: getToken(newUser)
         })
 
     } catch (error) {
-        res.status(500).send({message : error.message})
+        res.status(500).send({ message: error.message })
     }
 
 })
 
 
 
-userRouter.post("/login" , async (req ,res)=>{
-    
-    
+userRouter.post("/login", async (req, res) => {
+
+
     try {
         const name = req.body.name
         const roll = req.body.rollNo
-        const user = await Student.findOne({ rollNo : roll})
-        
-        if(user){
+        const user = await Student.findOne({ rollNo: roll })
+
+        if (user) {
 
             res.send({
-                    _id:user._id,
-                    name : user.name,
-                    rollNo : user.rollNo,
-                    branch : user.branch,
-                    cr : user.cr,
-                    sem : user.sem,
-                    isAdmin : user.isAdmin,
-                    token :getToken(user)
+                _id: user._id,
+                name: user.name,
+                rollNo: user.rollNo,
+                branch: user.branch,
+                cr: user.cr,
+                sem: user.sem,
+                isAdmin: user.isAdmin,
+                token: getToken(user)
             })
         }
-        else{
-            res.status(404).send({message : "Not found ,are you registered?"})
+        else {
+            res.status(404).send({ message: "Not found ,are you registered?" })
         }
     } catch (error) {
-        res.status(500).send({message : error.message})
+        res.status(500).send({ message: error.message })
     }
-       
+
 })
 
 

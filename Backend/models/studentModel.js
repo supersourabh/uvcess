0bf059257f32e31332a95c9e230a7d9717d4769c
supersourabh <70961689+supersourabh@users.noreply.github.com>
2021-10-19 15:36:11 +0530

    Update studentModel.js

--- Backend/models/studentModel.js
+++ Backend/models/studentModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const StudentSchema = new mongoose.Schema({
     name: { type: String, require: true  },
-    rollNo: { type: String, require: true, unique: true ,sparse: true , minlength},
+    rollNo: { type: String, require: true, unique: true ,sparse: true , minlength : 10 },
     branch: { type: String, require: true },
     contact: { type: String, unique: true, default: null, sparse: true },
     cr: { type: Boolean, default: false },
